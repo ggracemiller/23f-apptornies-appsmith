65d9a0fcbf2011b3a8a3ffcf8ee2ec7a11eca010
Nathan Kirschner <kirschner.n@northeastern.edu>
2023-12-05 20:02:20 +0000

    added links to all personas

--- pages/ChoosePersona/jsobjects/JSObject1/JSObject1.js
+++ pages/ChoosePersona/jsobjects/JSObject1/JSObject1.js
@@ -4,8 +4,13 @@ export default {
 	navigateOnSelect () {
 		console.log(Select1.selectedOptionValue)
 		if (Select1.selectedOptionValue === "Lawyer") {
-			console.log('here')
 			navigateTo('LawyerChooseName', {}, 'SAME_WINDOW');
+		}
+		else if (Select1.selectedOptionValue === "Client") {
+			navigateTo('Client_Information', {}, 'SAME_WINDOW');
+		}
+		else if (Select1.selectedOptionValue === "Partner") {
+			navigateTo('Full access to firm', {}, 'SAME_WINDOW');
 		}
 		//	write code here
 		//	this.myVar1 = [1,2,3]
